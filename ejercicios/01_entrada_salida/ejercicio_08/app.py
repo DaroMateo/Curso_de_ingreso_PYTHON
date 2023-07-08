5c71b0e847a2273a8c9a58b4598ac767f00bf18d
DaroMateo <138174735+DaroMateo@users.noreply.github.com>
2023-07-08 11:01:32 -0300

    Actualizar app.py eje8

--- ejercicios/01_entrada_salida/ejercicio_08/app.py
+++ ejercicios/01_entrada_salida/ejercicio_08/app.py
@@ -56,7 +56,7 @@ class App(customtkinter.CTk):
         self.txt_operador_a.delete(0,100)
         self.txt_operador_a.insert(0,"El resto de dividir es" + OperadorA + "y" + OperadorB + "es:" + resto_str)
 
-        alert("El resto de dividir es" + OperadorA + "y" + OperadorB + "es:" + resto_str)
+        alert("Resultado" , ("El resto de dividir es" + OperadorA + "y" + OperadorB + "es:" + resto_str))
     
         
         
