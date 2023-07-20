bf61fbaff1df4654c45b798e040739fb7a6d48bd
DaroMateo <138174735+DaroMateo@users.noreply.github.com>
2023-07-20 18:32:18 -0300

    Update For_app_03.py

--- ejercicios/05_instruccion_for/ejercicio_03/For_app_03.py
+++ ejercicios/05_instruccion_for/ejercicio_03/For_app_03.py
@@ -30,7 +30,7 @@ class App(customtkinter.CTk):
 
     def btn_mostrar_on_click(self):
         mensaje = "Hola UTN FRA"
-        for i in range(0,len(mensaje):
+        for i in range(0,5,1):
             alert("", mensaje)
 
         
