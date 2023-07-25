f952cb82afa03ee656670022ef2fab273c24a2bd
DaroMateo <138174735+DaroMateo@users.noreply.github.com>
2023-07-24 21:09:53 -0300

    Update List_app_01.py

--- ejercicios/06_listas/ejercicio_01/List_app_01.py
+++ ejercicios/06_listas/ejercicio_01/List_app_01.py
@@ -24,7 +24,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        for numero in self.lista_datos:
+            alert("", numero)
         
         
     
